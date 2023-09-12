3f601e81094c10ffc1576b8b10922eb114db94e6
gaborkiacz <kiacz.gabor@gmail.com>
2023-09-12 11:29:29 +0200

    Initial Commit

---
+++ NewsMoleRat/NewsMoleRatApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsMoleRatApp.swift
+//  NewsMoleRat
+//
+//  Created by Kiacz Gabor on 2023. 09. 12..
+//
+
+import SwiftUI
+
+@main
+struct NewsMoleRatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
