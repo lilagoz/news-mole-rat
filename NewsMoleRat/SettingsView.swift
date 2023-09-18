1827dc03f39ecf2d51a09b5784ce5ce71eb42322
gaborkiacz <kiacz.gabor@gmail.com>
2023-09-18 11:08:58 +0200

    experiments in settingsview

--- NewsMoleRat/SettingsView.swift
+++ NewsMoleRat/SettingsView.swift
@@ -7,32 +7,72 @@
 
 import SwiftUI
 
+struct Foo {
+    var name: String
+}
+
+class Bar {
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+    deinit {
+        print("bar deinit")
+    }
+}
+
 struct SettingsView: View {
-    @ObservedObject var config: Config
+    @State var foo: Foo = Foo(name: "Foo gizmo")
+    let bar: Bar = Bar(name: "Bar gadget")
+
+    func modifyFoo(fooParam: inout Foo ) {
+        fooParam.name = "modified foo 🐶"
+    }
+    
+    func modifyBar(barParam: Bar) {
+        barParam.name = "modified bar 🐎"
+    }
     
     func doSomething() {
-        config.allat = "macska"
+        
+
+        print("init foo: \(foo.name)")
+        print("init bar: \(bar.name)")
+        
+        foo.name = "🍩"
+        print("foo: \(foo.name)")
+        
+        modifyFoo(fooParam: &foo)
+        print("foo: \(foo.name)")
+
+        bar.name = "🍓"
+        print("bar:\(bar.name)")
+        modifyBar(barParam: bar)
+        print("bar:\(bar.name)")
     }
     
     init() {
-        self.config = .init()
-        doSomething()
+        //doSomething()
+        
     }
     
     var body: some View {
         VStack{
 //            TextField("Search...", text: $config.allat)
-            Text("Hello, World! \(config.allat)")
-            Form {
-                TextField(text: $config.allat, prompt: Text("Required")) {
-                    Text("Username")
-                }
-                SecureField(text: $config.allat, prompt: Text("Required")) {
-                    Text("Password")
-                }
+            Text("Hello, World!")
+            Button("do something") {
+                self.doSomething()
+            }
+//            Form {
+//                TextField(text: $config.allat, prompt: Text("Required")) {
+//                    Text("Username")
+//                }
+//                SecureField(text: $config.allat, prompt: Text("Required")) {
+//                    Text("Password")
+//                }
             }
             Spacer()
-        }
 
         
     }
